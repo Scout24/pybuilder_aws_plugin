f6f8ad2e615dcb7f1d0178a5f8398aa21c0a4310
Valentin Haenel <valentin@haenel.co>
2015-10-27 08:52:51 +0100

    activate coverage now that we have tests

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
-# use_plugin("python.coverage")
+use_plugin("python.coverage")
 use_plugin("python.distutils")
 
 
