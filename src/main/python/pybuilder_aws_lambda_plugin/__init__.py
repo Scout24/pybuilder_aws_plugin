b5f3942b1304761b3a8a7aa0bd0b02c7a30d318a
crose <carsten.rose@immobilienscout24.de>
2015-11-10 16:35:51 +0100

    remove check on importing cfn-sphere as we require it anyway when we run py2.7

--- src/main/python/pybuilder_aws_lambda_plugin/__init__.py
+++ src/main/python/pybuilder_aws_lambda_plugin/__init__.py
@@ -100,20 +100,15 @@ def upload_zip_to_s3(project, logger):
     upload_helper(logger, bucket_name, keyname_version, data, acl)
     upload_helper(logger, bucket_name, keyname_latest, data, acl)
 
-if sys.version_info[0:2] >= (2, 7):
+if sys.version_info[0:2] == (2, 7):
     @task('upload_json_to_s3',
           description="Convert & upload CloudFormation templates in JSON "
           "created out of the CFN-Sphere template YAML files")
     def upload_json_to_s3(project, logger):
-        try:
-            from cfn_sphere.aws.cloudformation.template_loader import (
-                CloudFormationTemplateLoader)
-            from cfn_sphere.aws.cloudformation.template_transformer import (
-                CloudFormationTemplateTransformer)
-        except ImportError:
-            logger.info(
-                "Unable to upload templates to S3. Could not find CFN-Sphere in path.")
-            exit(0)
+        from cfn_sphere.aws.cloudformation.template_loader import (
+            CloudFormationTemplateLoader)
+        from cfn_sphere.aws.cloudformation.template_transformer import (
+            CloudFormationTemplateTransformer)
 
         for path, filename in project.get_property('template_files'):
             template = CloudFormationTemplateLoader.get_template_from_url(
@@ -126,7 +121,8 @@ if sys.version_info[0:2] >= (2, 7):
             key_prefix = project.get_property('template_key_prefix')
             version_path = '{0}v{1}/{2}'.format(
                 key_prefix, project.version, filename.replace('yml', 'json'))
-            latest_path = '{0}latest/{1}'.format(key_prefix, filename.replace('yml', 'json'))
+            latest_path = '{0}latest/{1}'.format(
+                key_prefix, filename.replace('yml', 'json'))
 
             acl = project.get_property('template_file_access_control')
             upload_helper(logger, bucket_name, version_path, output, acl)
