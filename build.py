eb9917d0c72caeeab5a482d8e3ccb1a7d28b4470
Valentin Haenel <valentin@haenel.co>
2015-12-21 13:04:13 +0100

    fix imports from cfn-sphere after new version

--- build.py
+++ build.py
@@ -28,7 +28,7 @@ def set_properties(project):
     project.set_property('install_dependencies_upgrade', True)
     project.depends_on("boto3")
     if sys.version_info[0:2] >= (2, 7):
-        project.depends_on('cfn-sphere')
+        project.depends_on('cfn-sphere', '>=0.1.21')
     project.build_depends_on("mock")
     project.build_depends_on("moto")
     project.set_property('coverage_break_build', False)
