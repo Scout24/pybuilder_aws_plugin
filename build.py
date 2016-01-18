44085fe518396ecb491369628442f35a8fe6efdd
Valentin Haenel <valentin@haenel.co>
2016-01-18 15:08:44 +0100

    bug in httpretty fixed: https://github.com/gabrielfalcao/HTTPretty/issues/278#event-517939481

--- build.py
+++ build.py
@@ -29,7 +29,7 @@ def set_properties(project):
     project.depends_on("boto3")
     if sys.version_info[0:2] >= (2, 7):
         project.depends_on('cfn-sphere', '>=0.1.21')
-    project.depends_on('httpretty', '<=0.8.10')
+    project.depends_on('httpretty')
     project.build_depends_on("mock")
     project.build_depends_on("moto")
     project.set_property('coverage_break_build', False)
