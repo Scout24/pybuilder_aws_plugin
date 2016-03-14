634254f8b3e2667fac25af223cc70b26e94b9633
Schlomo Schapiro <schlomo+git@schapiro.org>
2016-03-14 15:11:35 +0100

    Add TeamCity build result info for release_custom_resource task

--- src/main/python/pybuilder_aws_plugin/helpers.py
+++ src/main/python/pybuilder_aws_plugin/helpers.py
@@ -48,3 +48,16 @@ def teamcity_helper(tc_param, keyname):
             "##teamcity[setParameter name='{0}' value='{1}']".format(
                     tc_param, keyname
             ))
+
+# copied from https://github.com/JetBrains/teamcity-messages/blob/942584909b2ce51e62a7c3360c5e1d1af6e1ef5a/teamcity/messages.py#L13
+_quote = {"'": "|'", "|": "||", "\n": "|n", "\r": "|r", '[': '|[', ']': '|]'}
+
+
+def _teamcity_escape_value(value):
+    return "".join(_quote.get(x, x) for x in value)
+
+
+def teamcity_append_build_status(text):
+    flush_text_line(
+        "##teamcity[buildStatus text='{build.status.text} %s']" % _teamcity_escape_value(text)
+    )
